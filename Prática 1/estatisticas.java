c426182d9b8eb141507cc4ef9ee5fa1079aee6ed
DiogoManim <diogomanimvila@gmail.com>
2023-12-03 15:05:09 +0000

    Adição das aulas práticas

---
+++ Prática 1/estatisticas.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class estatisticas {
+    static void media(int v[], int n) {
+        float soma = 0;
+
+        for (int i = 0; i < n; i++){
+            soma += v[i];
+        }
+
+        System.out.printf("%.2f\n", soma / n);
+    }
+
+    static void amplitude(int v[]) {
+        int min = v[0];
+        int max = v[0];
+
+        for (int i = 1; i < v.length; i++) {
+            if (v[i] > max) {
+                max = v[i];
+            }
+            else if (v[i] < min) {
+                min = v[i];
+            }
+        }
+
+        System.out.println(max-min);
+    }
+
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+
+        int n = stdin.nextInt();
+        int v[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            v[i] = stdin.nextInt();
+        }
+
+        media(v, n);
+        amplitude(v);
+    }
+}
