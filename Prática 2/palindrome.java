e8e1d5e5007c7419480409e5f7b8e83421d1fafa
DiogoManim <diogomanimvila@gmail.com>
2023-12-05 14:46:20 +0000

    update

--- Prática 2/palindrome.java
+++ Prática 2/palindrome.java
@@ -3,22 +3,17 @@ import java.util.Scanner;
 public class palindrome {
 
     public static boolean isPalindrome(String s) {
-        
         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         
-        int n = s.length();
+        int len = s.length();
 
-        for (int i = 0, j = 0; i < n; i++, j--) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) {
-                return false;
-            }
-        }
+        
+
         return true;
     }
 
     public static void main(String[] args) {
-        
-        java.util.Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
         System.out.println(n);
