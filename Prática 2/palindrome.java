8ea3bc9cc6da4916214367671443d81496327fe2
DiogoManim <d.manim.scp@gmail.com>
2023-12-06 23:03:36 +0000

    update

--- Prática 2/palindrome.java
+++ Prática 2/palindrome.java
@@ -3,37 +3,41 @@ import java.util.Scanner;
 public class palindrome {
 
     public static boolean isPalindrome(String s) {
-        
         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         
         int n = s.length();
 
-        for (int i = 0, j = 0; i < n; i++, j--) {
+        for (int i = 0; i < n/2; i++) {
             if (s.charAt(i) != s.charAt(n - i - 1)) {
                 return false;
             }
         }
+
         return true;
     }
 
     public static void main(String[] args) {
-        
-        java.util.Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+
         int n = in.nextInt();
+        in.nextLine();
+
+        String[] s = new String[100];
+
+        for(int i = 0; i < n; i++) {
+            s[i] = in.nextLine();
+        }
 
         System.out.println(n);
 
         for (int i = 0; i < n; i++) {
-
-            String s = in.nextLine();
-
-            if (isPalindrome(s))
+            if (isPalindrome(s[i]))
                 System.out.println("sim");
             else
                 System.out.println("nao");
+
         }
 
         in.close();
     }
-
 }
