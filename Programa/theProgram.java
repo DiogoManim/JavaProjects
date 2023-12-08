9b172338a501224f2cbac7d0db74009b477b0cd7
DiogoManim <d.manim.scp@gmail.com>
2023-12-08 00:13:06 +0000

    Programa (juro que não tem a ver com o tiagovski)

---
+++ Programa/theProgram.java
@@ -0,0 +1,13 @@
+public class theProgram {
+    public static void main(String[] args) {
+        vski prozis = new vski();
+
+        System.out.println(prozis.viewsYoutube());
+        
+        System.out.println(prozis.getX());
+
+        prozis.setX(68);
+
+        System.out.println(prozis.getX());
+    }
+}
---
+++ Programa/vski.java
@@ -0,0 +1,26 @@
+public class vski {
+    private int x;
+    private int y;
+
+    public vski() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public vski(int xvalue, int yvalue) {
+        this.x = xvalue;
+        this.y = yvalue;
+    }
+
+    public int viewsYoutube() {
+        return 12000000;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
